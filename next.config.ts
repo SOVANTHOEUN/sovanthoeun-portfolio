13dcdf661cfd80680675171d4b3ff42dc3cc8abc
sovanthoeun <sovanthoeun88@gmail.com>
2025-05-01 20:26:32 +0900

    Setup GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/sovanthoeun-portfolio',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
