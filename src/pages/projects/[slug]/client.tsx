efd952a52fa4aa77a4046e181d6c4c979835f54e
sovanthoeun <sovanthoeun88@gmail.com>
2025-07-12 00:12:02 +0900

    Fix header export

--- src/pages/projects/[slug]/client.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ClientPage = () => {
-  return <div />;
-};
-
-export default ClientPage;
--- src/pages/projects/[slug]/scene.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ScenePage = () => {
-  return <div />;
-};
-
-export default ScenePage;
